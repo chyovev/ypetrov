23a034aacfee2de9a4c8601f5524a9544d867f67
Christo Yovev <yovev@xroads-bg.com>
2021-02-03 10:58:32 +0100

    Add design responsiveness

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,9 @@ var App = {
     bind: function() {
         $(window).scroll(App.toggleStickyNavigation);
         $('.search-submit').on('click', App.showSearchField);
+        $('.nav-toggler-wrapper').on('click', App.toggleNavigation);
+        $('nav li.has-items > a').on('click', App.toggleSubNavigation);
+        $('.aside-toggler').on('click', App.togglePanel)
     },
     
     ///////////////////////////////////////////////////////////////////////////
@@ -29,13 +32,59 @@ var App = {
         // otherwise just submit the form
         if ( ! $('.search-field:visible').length) {
             e.preventDefault();
-            $('.logo-wrapper span').addClass('hidden');
+            $('.logo-wrapper span').animate({width:'toggle'}, 350);
             $('.search-field').animate({width:'toggle'}, 350).focus();
         }
-    }
+    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    toggleNavigation: function(e) {
+        $('#nav-toggler').toggleClass('active');
+        $('nav').slideToggle('normal', function() {
+            $(this).toggleClass('open').removeAttr('style');
+        });
+    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    // click to toggle only on mobile, desktop uses the hover CSS effect
+    toggleSubNavigation: function(e) {
+        if (App.isMobile() && $(this).siblings('ul').length) {
+            e.preventDefault();
+
+            // if the submenu is not visible, slide it down
+            if ( ! $(this).siblings('ul').is(':visible')) {
+                $(this).siblings('ul').slideDown('fast');
+            }
+            
+            // otherwise, slide it up AND remove the style="display: none" attr
+            // to guarantee desktop compatibility
+            else {
+                $(this).siblings('ul').slideUp('fast', function() {
+                    $(this).removeAttr('style');
+                });
+            }
+            
+            $(this).parent().toggleClass('open');
+
+            return false;
+        }
+        return true;
+    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    isMobile: function() {
+        return $('.mobile-only:visible').length ? true : false;
+    },
+
+    ///////////////////////////////////////////////////////////////////////////
+    togglePanel: function() {
+        $('.aside-wrapper').toggleClass('open');
+        $(this).toggleClass('open');
+    },
 }
 
 
 $(document).ready(function() {
     App.init();
+    App.toggleStickyNavigation();
 });
