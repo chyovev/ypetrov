553517ad79edebb60f95806fe5dc2dc6d1388674
Christo Yovev <yovev@xroads-bg.com>
2023-09-11 19:58:22 +0200

    Turn off model events on all commentable models' seeders

--- database/seeders/PoemSeeder.php
+++ database/seeders/PoemSeeder.php
@@ -3,10 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\Poem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PoemSeeder extends Seeder
 {
+
+    /**
+     * There is a dynamic polymorphic observer listening
+     * for delete event on Commentable models (such as Poem),
+     * but it should not be fired during seeding.
+     * 
+     * @see \App\Models\Traits\HasComments :: initializeHasComments()
+     */
+    use WithoutModelEvents;
 
     ///////////////////////////////////////////////////////////////////////////
     /**
--- database/seeders/PressArticleSeeder.php
+++ database/seeders/PressArticleSeeder.php
@@ -3,10 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\PressArticle;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PressArticleSeeder extends Seeder
 {
+
+    /**
+     * There is a dynamic polymorphic observer listening
+     * for delete event on Commentable models (such as PressArticle),
+     * but it should not be fired during seeding.
+     * 
+     * @see \App\Models\Traits\HasComments :: initializeHasComments()
+     */
+    use WithoutModelEvents;
 
     ///////////////////////////////////////////////////////////////////////////
     /**
