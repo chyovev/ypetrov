9a0aa0fad3d932a523e18ee49e611d81f7dcb911
Christo Yovev <yovev@xroads-bg.com>
2023-11-20 19:50:09 +0100

    Add default username to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,6 +13,40 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void {
+        $this->createPrimaryUser();
+        $this->createOtherUsers();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    /**
+     * It is essential that the primary user has a known username
+     * and password in order to guarantee access to the CMS to
+     * developers.
+     * 
+     * NB! Since there's a unique constraint on the email column,
+     *     but the seeder can be executed multiple times, it's
+     *     wise to insert said primary user only the first time
+     *     around.
+     */
+    private function createPrimaryUser(): void {
+        $name     = 'Administrator';
+        $email    = 'email@example.com';
+        $password = 'password';
+
+        if ( ! User::where('email', $email)->exists()) {
+            User::factory()->create([
+                'name'     => $name,
+                'email'    => $email,
+                'password' => $password, // password gets hashed automatically
+            ]);
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    /**
+     * Create two more users with faker data.
+     */
+    private function createOtherUsers(): void {
         User::factory(2)->create();
     }
 }
