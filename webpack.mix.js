3351e98afeb415b2aa7e2ba0f5265addbe2e7e3d
Christo Yovev <yovev@xroads-bg.com>
2023-12-04 19:09:17 +0100

    Copy Less stylesheet from main branch (alongside images and fonts), split it into smaller chunks & add them to the Laravel mix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,8 @@ let mix = require('laravel-mix');
 
 mix
     .options({ manifest: false });
+
+// all local fonts & images referenced in the less files
+// are automatically copied to the respective public folders
+mix
+    .less('resources/css/less/style.less', 'public/css/style.css');
