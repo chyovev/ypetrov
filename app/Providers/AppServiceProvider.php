7856632b74281680a0e3db3472abb191389e5362
Christo Yovev <yovev@xroads-bg.com>
2026-05-20 11:23:19 +0200

    Drop middleware extensions in favour of fine-tuned default Laravel ones

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,8 +6,12 @@ use View;
 use Blade;
 use Carbon\Carbon;
 use App\View\Composers\NavigationComposer;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Foundation\Http\Middleware\TrimStrings;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +33,9 @@ class AppServiceProvider extends ServiceProvider
         $this->setAdminPaginationTheme();
         $this->registerNavigationViewComposer();
         $this->registerMetaBladeDirectives();
+        $this->defineTrimStringsExemptions();
+        $this->overrideUnauthenticatedRedirect();
+        $this->overrideAuthenticatedRedirect();
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -105,4 +112,54 @@ class AppServiceProvider extends ServiceProvider
             ?>';
         });
     }
+
+    ///////////////////////////////////////////////////////////////////////////
+    /**
+     * As the name suggests, the TrimStrings middleware takes care
+     * of trimming all strings of an incoming request. In most
+     * cases that's okay, but some poems make use of leading
+     * white-spaces for visual text alignment.
+     * Since the exemption should apply only to the text field,
+     * the skipWhen is used to detect the current request, but
+     * its result is always false (i.e. no skipping) – instead,
+     * the text field gets added to the except list.
+     */
+    private function defineTrimStringsExemptions(): void {
+        TrimStrings::skipWhen(function(Request $request): bool {
+            if ($request->routeIs('admin.poems.*')) {
+                TrimStrings::except(['text']);
+            }
+
+            return false;
+        });
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    /**
+     * When an unauthenticated user tries to access a guarded route,
+     * they get automatically redirected to the login page by the
+     * Authenticate middleware.
+     * By default it uses a route called 'login' which does not
+     * exist here, so it should be overridden to 'admin.login'.
+     */
+    private function overrideUnauthenticatedRedirect(): void {
+        Authenticate::redirectUsing(function(Request $request): string {
+            return route('admin.login');
+        });
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    /**
+     * Similarly to the Authenticate middleware, the RedirectIfAuthenticated
+     * one redirects authenticated users to the home page in case they try
+     * to access a guest-only route.
+     * Since authentication matters only in the context of the admin panel,
+     * the home page should be the admin home page.
+     */
+    private function overrideAuthenticatedRedirect(): void {
+        RedirectIfAuthenticated::redirectUsing(function(Request $request): string {
+            return route('admin.home');
+        });
+    }
+
 }
